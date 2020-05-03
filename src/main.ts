f30509228a69147c910675ab3408ebe9e9f410e9
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2020-05-03 16:13:42 -0400

    write basic action

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,47 @@
 import * as core from '@actions/core'
-// import * as github from '@actions/github'
+import * as github from '@actions/github'
+import {findChecklists} from './utility'
 
 async function run(): Promise<void> {
   try {
-    
+    // get the issue
+    const issue = github.context.payload.issue
+    if (!issue) {
+      core.setFailed("No issue found on Actions context")
+      return
+    }
 
-    // const ms: string = core.getInput('milliseconds')
-    // core.debug(`Waiting ${ms} milliseconds ...`)
+    const checklistResult = findChecklists(issue.body)
+    if (checklistResult.open == 0 && checklistResult.closed == 0) {
+      console.log('No checklists found, bailing')
+      return
+    }
 
-    // core.debug(new Date().toTimeString())
-    // await wait(parseInt(ms, 10))
-    // core.debug(new Date().toTimeString())
+    const statusOpen = checklistResult.open > 0
+    const issueIsOpen = issue.state == 'open'
 
-    // core.setOutput('time', new Date().toTimeString())
+    if (statusOpen == issueIsOpen) {
+      console.log('Issue status unchanged, bailing')
+      return
+    }
+
+    // get the token
+    const token = core.getInput('github_token')
+    if (!token || token.length == 0) {
+      core.setFailed('Missing required github_token input')
+      return
+    }
+
+    const client = new github.GitHub(token);
+
+    console.log(`Setting issue status to ${statusOpen ? 'open' : 'closed'}...`)
+    await client.issues.update({
+      owner: github.context.payload.repository.owner.login,
+      repo: github.context.payload.repository.name,
+      issue_number: issue.number,
+      state: statusOpen ? 'open' : 'closed'
+    })
+    console.log(`Issue updated`)
   } catch (error) {
     core.setFailed(error.message)
   }
